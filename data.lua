de98d0c9df9e8da7b9b4cdef4d35c297b7017484
Cyphall <cyphallbraus@gmail.com>
2018-10-29 18:23:03 +0100

    1.1.0

--- data.lua
+++ data.lua
@@ -1,29 +1,27 @@
 data.raw["locomotive"]["locomotive"].max_speed = 0.6
-data.raw["locomotive"]["locomotive"].max_power = "250kW"
-data.raw["locomotive"]["locomotive"].burner.effectivity = 0.42
-data.raw["locomotive"]["locomotive"].braking_force = 3
 data.raw["locomotive"]["locomotive"].tie_distance = 25
 data.raw["locomotive"]["locomotive"].sound_minimum_speed = 0.25
 data.raw["locomotive"]["locomotive"].air_resistance = 0.0040
 data.raw["locomotive"]["locomotive"].friction_force = 0.125
+data.raw["locomotive"]["locomotive"].weight = 6000
 
 data.raw["cargo-wagon"]["cargo-wagon"].max_speed = 0.75
-data.raw["cargo-wagon"]["cargo-wagon"].braking_force = 1
 data.raw["cargo-wagon"]["cargo-wagon"].tie_distance = 25
 data.raw["cargo-wagon"]["cargo-wagon"].sound_minimum_speed = 0.25
 data.raw["cargo-wagon"]["cargo-wagon"].air_resistance = 0.0040
 data.raw["cargo-wagon"]["cargo-wagon"].friction_force = 0.125
+data.raw["cargo-wagon"]["cargo-wagon"].weight = 3000
 
 data.raw["fluid-wagon"]["fluid-wagon"].max_speed = 0.75
-data.raw["fluid-wagon"]["fluid-wagon"].braking_force = 1
 data.raw["fluid-wagon"]["fluid-wagon"].tie_distance = 25
 data.raw["fluid-wagon"]["fluid-wagon"].sound_minimum_speed = 0.25
 data.raw["fluid-wagon"]["fluid-wagon"].air_resistance = 0.0040
 data.raw["fluid-wagon"]["fluid-wagon"].friction_force = 0.125
+data.raw["fluid-wagon"]["fluid-wagon"].weight = 3000
 
 data.raw["artillery-wagon"]["artillery-wagon"].max_speed = 0.75
-data.raw["artillery-wagon"]["artillery-wagon"].braking_force = 1
 data.raw["artillery-wagon"]["artillery-wagon"].tie_distance = 25
 data.raw["artillery-wagon"]["artillery-wagon"].sound_minimum_speed = 0.25
 data.raw["artillery-wagon"]["artillery-wagon"].air_resistance = 0.0040
 data.raw["artillery-wagon"]["artillery-wagon"].friction_force = 0.125
+data.raw["artillery-wagon"]["artillery-wagon"].weight = 3000
